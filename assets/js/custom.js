bdfdba614ecf8d760d60caed1b1d6fb088e23825
nasrullah.mansur <nasrullah.cit.bd@gmail.com>
2021-10-24 20:12:11 +0600

    before responsive

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -8,6 +8,30 @@ $(document).ready(function() {
         nextArrow: '<button type="button" class="slick-next"><i class="fas fa-chevron-right"></i></button>',
     });
 
+    $('.search-page .slider-content').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-chevron-left"></i></button>',
+        nextArrow: '<button type="button" class="slick-next"><i class="fas fa-chevron-right"></i></button>',
+        dots: true,
+    });
+
+    $('.search-page .products .slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-chevron-left"></i></button>',
+        nextArrow: '<button type="button" class="slick-next"><i class="fas fa-chevron-right"></i></button>',
+        dots: true,
+    });
+
+    $('.search-page .related-product .product-slide').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-chevron-left"></i></button>',
+        nextArrow: '<button type="button" class="slick-next"><i class="fas fa-chevron-right"></i></button>',
+        dots: false,
+    });
+
     $('.faq .question-tab li').on('click', function() {
         $(this).addClass('active').siblings('li').removeClass('active');
         let getClass = '.faq .question-content .' + $(this).attr('data-tab');
